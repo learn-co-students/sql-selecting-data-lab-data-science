63710aeeb4e7d2888413ad7f928e715d33c9981a
Forest Polchow <fpolchow@gmail.com>
2018-07-06 17:15:32 -0400

    fixed bug in sql_runner.py
    changed ../ to ./

--- test/sql_runner.py
+++ test/sql_runner.py
@@ -6,13 +6,13 @@ class SQLRunner:
         self.cursor = self.connection.cursor()
 
     def execute_create_file(self):
-        file = open("../create.sql", "r")
+        file = open("./create.sql", "r")
         sql = file.read()
         table = self.cursor.execute(sql)
         return table
 
     def execute_seed_file(self):
-        file = open("../seed.sql", "r")
+        file = open("./seed.sql", "r")
         sql = file.read()
         table = self.cursor.execute(sql)
         return table
